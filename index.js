2b0ff65c4eaafaa60674fbe12e67db159ea6658a
Eight8 <66164881+Eight8-7020@users.noreply.github.com>
2020-10-30 11:54:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function({ config }) {
   const discord = require('discord.js');
-  const client = new Discord.Client;
+  const client = new discord.Client;
   
   // now you have a discord.js client object which you can do whatever to.
   // It's better just to use eris
